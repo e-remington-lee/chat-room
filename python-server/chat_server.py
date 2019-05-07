97343e6e60a20fe4b46da04e8c63f07eceb4b486
Erik Remington Lee <eriklee19@gmail.com>
2019-05-07 19:44:15 -0400

    added .gitignore

--- python-server/chat_server.py
+++ python-server/chat_server.py
@@ -1,9 +1,22 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
 
-@app.route('/')
-def hello_world():
-    return 'Hello, world!'
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+class Second(Resource):
+    def get(self):
+        return jsonify(username='Blaine',
+                  email='bdh22')
+
+api.add_resource(Second, '/2')
+
 
 if __name__  == '__main__':
     app.run(debug=True, port=8000)
