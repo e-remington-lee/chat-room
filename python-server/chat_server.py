d75ff64ad43ddfe81cbfabd335754e1f2970d205
Erik Remington Lee <eriklee19@gmail.com>
2019-05-07 22:20:13 -0400

    trying to serve angular project using flask

--- python-server/chat_server.py
+++ python-server/chat_server.py
@@ -1,22 +1,25 @@
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask import Flask, jsonify, render_template, send_from_directory
+# from flask_restful import Resource, Api
 
 app = Flask(__name__)
-api = Api(app)
+# api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+@app.route('/templates/index.html')
+def hello():
+    return send_from_directory('/templates', "index.html")
 
-api.add_resource(HelloWorld, '/')
+# @app.route('/templates/index.html')
+# def hello():
+#         return render_template('index.html')
 
-class Second(Resource):
-    def get(self):
-        return jsonify(username='Blaine',
-                  email='bdh22')
+# api.add_resource(Angular, '/')
 
-api.add_resource(Second, '/2')
+# class Second(Resource):
+#     def get(self):
+#         return jsonify(username='Blaine',
+#                   email='bdh22')
 
+# api.add_resource(Second, '/2')
 
 if __name__  == '__main__':
     app.run(debug=True, port=8000)
