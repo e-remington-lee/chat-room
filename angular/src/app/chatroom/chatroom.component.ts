54955be714926e3914318d60bc7142fe96fe7dcb
Erik Remington Lee <eriklee19@gmail.com>
2019-05-08 23:13:31 -0400

    python server and skeleton compelted

--- angular/src/app/chatroom/chatroom.component.ts
+++ angular/src/app/chatroom/chatroom.component.ts
@@ -9,14 +9,23 @@ export class ChatroomComponent implements OnInit {
 
   inputText: string;
   sendText: string;
+  userName: string;
+  info: Object;
+
   constructor() { }
 
   ngOnInit() {
+    this.userName='Blaine';
   }
 
   onEnter(){
+    this.info = {
+      "user":[
+        {"a": this.userName, "b": this.inputText}
+      ]
+    }
+
     this.sendText = this.inputText;
-    console.log(this.sendText);
   }
 
 }
--- python-server/chat_server.py
+++ python-server/chat_server.py
@@ -4,23 +4,19 @@ from flask import Flask, jsonify, render_template, send_from_directory
 app = Flask(__name__)
 # api = Api(app)
 
-@app.route('/templates/<bb>')
-def routers(bb):
-        print(bb)
-        return send_from_directory('templates', bb)
+# @app.route('/templates/<bb>')
+# def routers(bb):
+#         print(bb)
+#         return send_from_directory('templates', bb)
 
 @app.route('/')
 def root_server():
         return render_template('index.html')
 
-# api.add_resource(Angular, '/')
-
-# class Second(Resource):
-#     def get(self):
-#         return jsonify(username='Blaine',
-#                   email='bdh22')
-
-# api.add_resource(Second, '/2')
+#try this
+# @app.route('/')
+# def routers():
+#         return send_from_directory('/static', '/templates/index.html')
 
 if __name__  == '__main__':
-    app.run(debug=True, port=8002)
+    app.run(debug=True, port=8001)
