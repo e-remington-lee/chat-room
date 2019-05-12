f6210127f450d485a9d6487acafa7f481f9053c7
Erik Remington Lee <eriklee19@gmail.com>
2019-05-12 00:34:38 -0400

    Post request works, figuring out how to add dictionary to existing json data

--- python-server/chat_server.py
+++ python-server/chat_server.py
@@ -1,20 +1,17 @@
-from flask import Flask, jsonify, render_template, send_from_directory
+from flask import Flask, jsonify, render_template, request
 import json
 # from flask_restful import Resource, Api
 
 app = Flask(__name__)
 # api = Api(app)
-info = [
-      {"id": 1,
+info = {"id": 1,
         "username":"Nobody",
-       "message":"..."}, 
-      {"id": 2,
+       "message":"..."}, {"id": 2,
         "username":"Literally No one",
-       "message":"..."},
-      {"id": 3,
+       "message":"..."},{"id": 3,
         "username":"David Benioff",
-       "message":"Danny forgot about the Iron Fleet but the Iron Fleet did not forget about her"}
-        ]  
+       "message":"Danny forgot about the Iron Fleet but the Iron Fleet didn't forget about her"}
+     
 
 json_data = json.dumps(info)
 
@@ -22,9 +19,31 @@ json_data = json.dumps(info)
 def root_server():
         return render_template('index.html')
 
-@app.route('/users', methods=['GET', 'POST'])
+@app.route('/messages', methods=['GET'])
 def get_users():
-        return json_data
+        return jsonify(info)
+
+@app.route('/messages', methods=['POST'])
+def add_message():
+
+        data = request.get_json()
+
+        username = data['username']
+        message = data['message']
+        id = data['id']
+
+        # json_data.update({"username":username, "message":message, "id":id})
+        # return json_data
+
+        #how do I add a dictonary to already existing json data
+        return jsonify({"id": 1,
+        "username":"Nobody",
+       "message":"..."}, {"id": 2,
+        "username":"Literally No one",
+       "message":"..."},{"id": 3,
+        "username":"David Benioff",
+       "message":"Danny forgot about the Iron Fleet but the Iron Fleet didn't forget about her"},{"username":username, "message":message, "id":id})
+        
 
 if __name__  == '__main__':
     app.run(debug=True, port=8000)
