6232200ffa8b53b71b7695ff77f1d98f46805c77
Erik Remington Lee <eriklee19@gmail.com>
2019-05-11 23:22:05 -0400

    creating endpoints for chat server

--- python-server/chat_server.py
+++ python-server/chat_server.py
@@ -1,22 +1,30 @@
 from flask import Flask, jsonify, render_template, send_from_directory
+import json
 # from flask_restful import Resource, Api
 
 app = Flask(__name__)
 # api = Api(app)
+info = [
+      {"id": 1,
+        "username":"Nobody",
+       "message":"..."}, 
+      {"id": 2,
+        "username":"Literally No one",
+       "message":"..."},
+      {"id": 3,
+        "username":"David Benioff",
+       "message":"Danny forgot about the Iron Fleet but the Iron Fleet did not forget about her"}
+        ]  
 
-# @app.route('/templates/<bb>')
-# def routers(bb):
-#         print(bb)
-#         return send_from_directory('templates', bb)
+json_data = json.dumps(info)
 
 @app.route('/')
 def root_server():
         return render_template('index.html')
 
-#try this
-# @app.route('/')
-# def routers():
-#         return send_from_directory('/static', '/templates/index.html')
+@app.route('/users', methods=['GET', 'POST'])
+def get_users():
+        return json_data
 
 if __name__  == '__main__':
     app.run(debug=True, port=8000)
