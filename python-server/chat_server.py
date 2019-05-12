78dff84fe2ae352674236e66063638f4f7ffdd67
Erik Remington Lee <eriklee19@gmail.com>
2019-05-12 18:55:18 -0400

    working on returning json data from user get/post request

--- python-server/chat_server.py
+++ python-server/chat_server.py
@@ -1,12 +1,25 @@
 from flask import Flask, jsonify, render_template, request
 import json
-# from flask_restful import Resource, Api
 
 app = Flask(__name__)
-# api = Api(app)
-info ='''{"message_history": [{"id": 1,"username":"Nobody","message":"..."},{"id": 2,"username":"Literally No one","message":"..."
-                },{"id": 3,"username":"David Benioff","message":"Danny forgot about the Iron Fleet but the Iron Fleet didn't forget about her"
-                },{"id": 4,"username":"David Benioff",
+
+info ='''{"message_history": [
+                {
+                "id": 1,
+                "username":"Nobody",
+                "message":"..."
+                },
+                {"id": 2,
+                "username":"Literally No one",
+                "message":"..."
+                },
+                {
+                "id": 3,
+                "username":"David Benioff",
+                "message":"Danny may have forgot about the Iron Fleet, but the Iron Fleet didn't forget about her"
+                },
+                {"id": 4,
+                "username":"David Benioff",
                 "message":"Duplicate user test"
                 }
        ]
@@ -38,14 +51,24 @@ def find_create_users():
         if request.method =='GET':
                 all_users =[]
                 
-                for message in json_data['message_history']:
-                        all_users.append(message['username'])
+                for user in json_data['message_history']:
+                        all_users.append(user['username'])
 
                 unique_users = list(set(all_users))
 
-                return jsonify(unique_users), print(unique_users)
+                return jsonify(unique_users)
         elif request.method =='POST':
-                return None
+                data = request.get_json()
+                username = data['username']
+
+                json_data['message_history'].append({"username":username})
+                return jsonify(json_data), 201
+
+@app.route('/messages/*', methods=['GET'])
+def find_specific_user():
+
+
+        return None
 
 if __name__  == '__main__':
     app.run(debug=True, port=8000)
