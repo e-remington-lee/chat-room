63b4265847f83802905ee004679e150b0944303d
Erik Remington Lee <eriklee19@gmail.com>
2019-05-14 20:45:39 -0400

    working endpoints to search for specific user, and proper dummy data

--- python-server/chat_server.py
+++ python-server/chat_server.py
@@ -5,21 +5,21 @@ import json
 app = Flask(__name__)
 
 user_list = []
-user_list.append({"id": 1,"username":"Nobody"})
-user_list.append({"id": 2,"username":"Literally No One"})
-user_list.append({"id": 3,"username":"David Benioff"})
-user_list.append({"id": 4,"username":"Duplicate"})
+user_list.append({"user_id": 1,"username":"Nobody"})
+user_list.append({"user_id": 2,"username":"Literally No One"})
+user_list.append({"user_id": 3,"username":"David Benioff"})
+user_list.append({"user_id": 4,"username":"Duplicate"})
 
 user_data = {
         "user_list": user_list
 }
 
 message_history = []
-message_history.append({"id": 1,"username":"Nobody","message":"..."})
-message_history.append({"id": 2,"username":"Literally No One","message":"..."})
-message_history.append({"id": 3,"username":"David Benioff","message":"Danny may have forgot about the Iron Fleet, but the Iron Fleet didn't forget about her"})
-message_history.append({"id": 4,"username":"Duplicate","message":"Test 1"})
-message_history.append({"id": 5,"username":"Duplicate","message":"Test 2"})
+message_history.append({"id": 1,"user":{"user_id": 1,"username":"Nobody"},"message":"..."})
+message_history.append({"id": 2,"user":{"user_id": 2,"username":"Literally No One"},"message":"..."})
+message_history.append({"id": 3,"user":{"user_id": 3,"username":"David Benioff"},"message":"Danny may have forgot about the Iron Fleet, but the Iron Fleet didn't forget about her"})
+message_history.append({"id": 4,"user":{"user_id": 4,"username":"Duplicate"},"message":"Test 1"})
+message_history.append({"id": 5,"user":{"user_id": 4,"username":"Duplicate"},"message":"Test 2"})
 
 message_data ={
         "message_history": message_history
@@ -34,31 +34,36 @@ def get_post_messages():
                 return jsonify(message_data)
         elif request.method =='POST':
                 data = request.get_json()
-                username = data['username']
+                user = data['user']
                 message = data['message']
                 id = len(message_data['message_history'])+1
 
-                message_data['message_history'].append({"username":username, "message":message, "id":id})
+                message_data['message_history'].append({"user":user, "message":message, "id":id})
                 
                 return jsonify(message_data), 201
 
 @app.route('/users', methods=['GET','POST'])
 def find_create_users():
         if request.method =='GET':
-                return jsonify(user_data)
+                q = request.args.get('user_id')
+                if q:
+                        for user in user_data['user_list']:
+                                if str(user['user_id']) == q:
+                                        return jsonify(user)
+
+                        return jsonify({"user_id":"N/A", "username": "Not Found"}), 404
+                else:
+                        print(q)
+                        print(request.query_string)
+                        return jsonify(user_data)
         elif request.method =='POST':
                 data = request.get_json()
                 username = data['username']
                 id = len(user_data["user_list"])+1
 
-                user_data['user_list'].append({"username":username, "id":id})
+                user_data['user_list'].append({"username":username, "user_id":id})
 
                 return jsonify(user_data), 201
-
-@app.route('/messages/q={"filters":[{"name":"id","op":"eq", "val":2}]}', methods=['GET'])
-def find_specific_user():
-        if request.method == 'GET':
-                return request.get_json()
 
 if __name__  == '__main__':
     app.run(debug=True, port=8000)
