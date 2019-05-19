cc1e04d467065a5b504aad78f9d5ca4e257d7ee2
Erik Remington Lee <eriklee19@gmail.com>
2019-05-19 01:47:45 -0400

    databse successfuly retrieving and and writing data

--- python-server/data_server.py
+++ python-server/data_server.py
@@ -1,2 +1,28 @@
-#this is the data server for prostgres
+import psycopg2
+import sqlalchemy
 
+connect = psycopg2.connect(
+        host = "localhost",
+        database = "Chat_room",
+        user = "postgres",
+        password = "Bob!3489")
+
+#how was I supposed to know the host, I just guessed at localhost
+
+user_databse = 'users'
+cursor = connect.cursor()
+
+cursor.execute(f"INSERT INTO {user_databse} VALUES (5, 'Jeff')")
+
+select_all = f'select * from {user_databse}'
+
+cursor.execute(select_all)
+rows = cursor.fetchall()
+
+for r in rows:
+    print(r)
+
+connect.commit()
+
+cursor.close()
+connect.close()
