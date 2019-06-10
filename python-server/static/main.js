d0404c843c06a2b9671cd80eb0acec5c6908df0d
Erik Remington Lee <eriklee19@gmail.com>
2019-06-09 21:31:07 -0400

    changed socket url

--- python-server/static/main.js
+++ python-server/static/main.js
@@ -363,7 +363,7 @@ __webpack_require__.r(__webpack_exports__);
 var WebsocketService = /** @class */ (function () {
     function WebsocketService() {
         var _this = this;
-        this.url = 'http://localhost:8000';
+        this.url = 'https://erl-chat-room.herokuapp.com/';
         this.receiveSocketMessages = function () {
             return rxjs__WEBPACK_IMPORTED_MODULE_3__["Observable"].create(function (observer) {
                 _this.socket.on('message', function (message) {
